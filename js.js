383e808ebf81754bb25b8ff950cb9e12fc1b9bb3
Akbarjon <akbarjonraxmonov@1997gmail.com>
2024-01-21 20:49:58 +0500

    js yangi masalalarim

--- js.js
+++ js.js
@@ -1,107 +1,90 @@
-//UYGA VAZIFA!**
-//     1. Foydalanuvchi biror bir son kiritadi. Dasturingiz kiritilgan son toq yoki juft ekanligini aniqlashi kerak.
+// - **Amailoyt!**
+// // 1. Kiritilgan son
+// //    100 dan katta yoki kichikligini aniqlab bersin.
 
-// let birirbirSon = +prompt("birortason kirgazing " , "")
+// let son = +prompt(`son kitgazing `," ")
 
-// if(birirbirSon % 2 === 0){
-//    document.write(`sizkirgizgan ${birirbirSon} juft son `)
-// }else{
-//    document.write(`sizkirgizgan ${birirbirSon} toq son `)
-// }
-
-//     2. Foydalanuvchi 3 ta son kiritadi. Shu uchta sonning ichidan eng kattasini ekranga chiqaring.
-
-// let birinchiSon = prompt("birinchiSon ni kirgazing " , "")
-// let ikkinchiSon = prompt("ikkinchiSon ni kirgazing " , "")
-// let uchinchiSon = prompt("uchinchiSon ni kirgazing " , "")
-
-// if(birinchiSon >= ikkinchiSon && birinchiSon>=uchinchiSon){
-//    document.write(`siz kiritgan ${birinchiSon}  eng kata` )
-// }else if(ikkinchiSon >= birinchiSon && ikkinchiSon>=uchinchiSon){
-//    document.write(`siz kiritgan ${ikkinchiSon}  eng kata`)
-// } else if(uchinchiSon >= birinchiSon && uchinchiSon>=ikkinchiSon){
-//    document.write(`siz kiritgan ${uchinchiSon}  eng kata`)
-// }else{
-//    document.write("xato")
-// }
+// if(son => 100 ){document.write(`siz kiritgan son ${son} => 100 dan kata `)}
+// else if(son <= 100 ){document.write(`siz kiritgan son ${son} =>  100 dan kichik `)}
+// else{document.write("xato malumot kirazdingiz ")}
 
 
-//     3. Foydalanuvchi biror son kiritadi. Kiritilgan son 3 ga bo'linsa ekranga "FIZZ", 5 ga bo'linsa "Buzz", 3ga va 5ga bo'linsa esa "FIZZBUZZ" degan yozuv chiqsin.
-  
-// let birirbirSon = +prompt("birortason kirgazing " , "")
 
-// if(birirbirSon % 3 === 0 && birirbirSon % 5 === 0){
-//   document.write("FIZZBUZZ")
-// } else if(birirbirSon % 5 === 0){
-//    document.write("Buzz")
-// }else if(birirbirSon % 3 === 0){
-//    document.write("FIZZ")
-// }else{
-//    document.write("xato")
-// }
+// // 2. Kiritilgan son
+// //    juft va 100dan katta bo'lsa "Tog'ri" degan so'z chiqarsin
+// //    aks holda "Noto'g'ri" degan so'zni chiqarsin.
 
-//     4. Foydalanuvchi yilni kiritadi. Dastur esa kiritilgan yil kabisa yilimi yoqmi aytishi kerak.
-// let son = +prompt(" yil kirgazing","")
+// let son = +prompt(`son kitgazing `," ")
 
-//  if(son % 4 == 0){
-//    if(son % 100 == 0){
-
-//       if(son % 400 == 0){
-//          document.write(`siz kiritgan yil ${son} kabissa yoli`)
-//       }
-//      document.write(`siz kiritgan yil ${son} kabissa yiliemas`)
-//    }
-//    document.write(`siz kiritgan yil ${son} kabissa yoli`)
-//  }
-//  else{
-//    document.write(`siz kiritgan yil ${son} kabissa yili emas`)
-//  }
-//     5. Foydalanuvchi n son kiritadi. n gacha bo'lgan sonlarning ko' paytamasini toping
-//     Masalan
-//     Input: 3
-//     Output: 6 (Chunki 1*2*3=6)
+// if(son % 2 == 0 && son > 100  ){document.write(`kiritgan son ${son}  => Tog'ri`)}
+// // else if(son <= 100 ){document.write(`siz kiritgan son ${son} =>  100 dan kichik `)}
+// else{document.write("Noto'g'ri")}
 
 
-//     6. Baxolash sistemasi. Ball (0 dan 100gacha) kiritilganda 5 yoki 4 yoki 3 yoki qolib ketganini chiqaruvchi dastur tuzib bering.
-
-// let ball = prompt("olgan ball ingizni kirgazing","")
-
-// if(ball > 60 && ball <= 72 ){document.write(`siz ${ball} oldingiz bu 3-baho`)}
-// else if(ball >=73 && ball <= 89){document.write(`siz ${ball} oldingiz bu 4-baho`)}
-// else if(ball >=90 && ball <= 100){document.write(`siz ${ball} oldingiz bu 5-baho`)}
-// else(document.write(`siz ${ball} ball oldingiz 3 baldan ham pas siz sesiyadan ota olmadingiz`))
 
 
-//     7. Agar soat 9:00 bilan 18:00 oraligвЂ™ida boвЂ™lganida вЂњOfis ochiqвЂќ, agar soat 18:00 bilan 9:00 oraligвЂ™ida boвЂ™lganida вЂњOfis yopiqвЂќ degan soвЂ™zni console oynasiga chiqarib beruvchi dastur tuzib bering. 
-
-// let Vaqt = prompt("vaqtni  kirgazing","")
-// if(Vaqt => 9 && Vaqt <= 18){document.write(`siz kirgizgan vaqtda ${Vaqt} oraligвЂ™ida boвЂ™lganida вЂњOfis ochiqвЂќ`)}
-// else if(Vaqt => 18 && Vaqt <= 9){document.write(`siz kirgizgan vaqtda ${Vaqt} oraligвЂ™ida boвЂ™lganida вЂњOfis yopiqвЂќ`)}
-// else(`ish vaqti emas  ....`)
+// // 3. Parol kiritilsin
+// //    kiritilgan parol "soff2023" ga teng bo'lsa "Hush kelibsiz"
+// //    aks holda parol noto'g'ri degan so'zni chiqarib bering
 
 
-//     8. Kiritilga yosh qaysi levelda ekanligni aniqlab beruvchi dastur yozib bering.
- 
-// document.getElementById(yosh)
-// let yosh = +prompt("yoshingizni kirgazing","")
-// if(yosh >= 1 && yosh <=3){document.write(`Chaqaloq в‡’${yosh} =>  1-3 oraligi`)}
-// else if(yosh >= 4 && yosh <=6){document.write(`Go'dak в‡’${yosh} =>   4-6 oraligi`)}
-// else if(yosh >= 7 && yosh <=12){document.write(`Nola в‡’${yosh} =>  7-12  oraligi`)}
-// else if(yosh >= 13 && yosh <=18){document.write(`O'smir в‡’${yosh}  =>  13-18 oraligi`)}
-// else if(yosh >= 18 && yosh <=20){document.write(`O'spirin в‡’${yosh} =>   18-20 oraligi`)}
-// else if(yosh >= 21 && yosh <=40){document.write(`Erkak в‡’${yosh} =>   21-40 oraligi`)}
-// else if(yosh >= 41 && yosh <=60){document.write(`Amaki в‡’${yosh} =>   41-60 oraligi`)}
-// else if(yosh>=60){document.write(`Bobo в‡’${yosh}  =>   60 + oraligi`)}
-// else(" siz notog'ri malumot kirgazdingiz !!" )
-        
-//         Chaqaloq в‡’ 1-3
-//         Go'dak  в‡’ 4-6
-//         Nola  в‡’  7-12
-//         O'smir  в‡’ 13-18
-//         O'spirin в‡’ 18-20
-//         Erkak в‡’ 21-40
-//         Amaki  в‡’  41 - 60
-//         Bobo в‡’ 60+
+// let parol = prompt("parol ,soff2023, kirgazing "," ")
+
+// if(parol === "soff2023" ){
+//    document.write(`Hush kelibsiz : `)
+// }else{document.write(`parol noto'g'ri `)}
+
+
+
+
+
+// // 4. Ikta son kiritlsin
+// //    Kiritilgan sonlar Ikkisi ham Toq Yoki Juft ekanligini aniqlang yoki
+// //    misol uchun: 5 va 7 => natija: ikkisi ham toq
+// //    misol uchun: 56 va 24 => natija: ikkisi ham Juft
+// //    misol uchun: 56 va 23 => natija: Juft ham Toq ham bor
+
+// let son1 = +prompt(`son kitgazing `," ")
+// let son2 = +prompt(`son kitgazing `," ")
+
+// if(son1 % 2 === 0 && son2 % 2 === 0){document.write(`${son1 } => ${son2} natija: ikkisi ham Juft`)}
+// else if(son1 % 2 === 1 && son2 % 2 === 1){document.write(`${son1 } => ${son2} natija: ikkisi ham toq`)}
+// else if(son1 % 2 === 1 || son2 % 2 === 1 && son1 % 3 === 0 || son2 % 3 === 0){document.write(`${son1 } => ${son2} jujt ham toq ham bor`)}
+// else{" hato malumot kirgazdindiz raqam kirgazing "}
+
+
+
+
+
+// // 5. Ikta son kiritlsin
+// //    Kiritilgan sonlarning juftisini 3 ga oshirib, toq sonini esa
+// //    3 karra kamaytirib beruvchi dastur tuzib bering.
+// //    misol uchun: 4 va 3 => natija: 7 va -6
+
+let son1 = +prompt(`son kitgazing `," ")
+let son2 = +prompt(`son kitgazing `," ")
+
+if(son1 % 2 == 0 || son1 % 3 == 0){
+   document.write(son1 + 3, ` => `,son1-10)
+} 
+else if(son2 % 2 == 0 || son2 % 3 == 0){
+   document.write( `${son2+3}`,` => `,`-${son2-10}` )
+}else{"xatao"}
+
+
+// - **UYGA VAZIFA!**
+//     - 1-masala
+//     Ikki xonali son berilgan. console oynasida birinchi o'nlik xonasidagi son, ikkinchi
+//     birlar xonasidagi son chiqarilsin va raqamlar yig'indisi xisoblansin
+//     - 2-masala
+//     Ikki xonali son berilgan. Uning raqamlar o'rni almashganda son hosil qilib bering. m-u: 25 в‡’ 52, 61 в‡’ 16, 94 в‡’ 49, v.k
+//     - 3-masala
+//     A kesma va B kesma berilgan. A kesma ichida B kesmadan necha marta joylashtirish mumkin?
+//     - 4-masala
+//     4 xonali sonlarni 1000lar 100lar 10lar va birlar xonasidagi sonni chiqaradigan dastur yasab bering.
+//     - Yuqoridagi misollardan tashqari darsda koвЂ™rilgan misollarni yana qayta oвЂ™zi mustaqil yozib tahlil qilib kelish.
+    
+//     QoвЂ™shimcha Ustoz tomonida boshqa misollar ham boвЂ™lsa berilishi tafsiya beriladi.
 
 
 
